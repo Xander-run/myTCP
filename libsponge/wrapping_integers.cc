3dad3fcd50400dfe0c6686ced4bed654f06b8bda
LTakamori <925951065@qq.com>
2021-05-26 22:58:53 +0800

    fuck u

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -9,13 +9,14 @@ template <typename... Targs>
 void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
+const static uint64_t TWO_POWER32 = 4294967295 + 1;
 
 //! Transform an "absolute" 64-bit sequence number (zero-indexed) into a WrappingInt32
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+    n = n + isn.raw_value();
+    return WrappingInt32(static_cast<uint32_t>(n));
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -29,6 +30,6 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    return 0;
+    // fuck u;
 }
